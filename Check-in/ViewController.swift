51f44763bd17a212cce6ca0b9ae4de4deca1b979
iDreams <idreams@iDreamss-MacBook-Pro.local>
2015-08-01 11:55:10 +0800

    Initial Commit

---
+++ Check-in/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check-in
+//
+//  Created by iDreams on 2015/8/1.
+//  Copyright (c) 2015年 HunagMay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
